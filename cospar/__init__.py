584b5e658e3989c13c8c0261b1e1934ce325adf8
ShouWenWang <wangsw09@gmail.com>
2021-04-15 21:51:47 -0400

    final proof reading

--- cospar/__init__.py
+++ cospar/__init__.py
@@ -1,6 +1,6 @@
 """CoSpar - dynamic inference by integrating transcriptome and lineage information"""
 
-__version__='0.1.3'
+__version__='0.1.4'
 from . import pl # plotting
 from . import tmap # transition map inference
 from . import hf # help functions
