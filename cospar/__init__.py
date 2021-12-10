6d2028717a048db7ad79b0cdb6f25910b6901eec
ShouWenWang <wangsw09@gmail.com>
2021-12-09 19:10:13 -0500

    Update version to be 0.2.0

--- cospar/__init__.py
+++ cospar/__init__.py
@@ -1,4 +1,4 @@
 """CoSpar - dynamic inference by integrating transcriptome and lineage information"""
 
-__version__ = "0.1.9"
+__version__ = "0.2.0"
 from . import datasets, hf, logging, pl, pp, settings, tl, tmap
