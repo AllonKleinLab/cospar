085af22bdceb55dcf8e0dde57fda3292211b7513
ShouWenWang <wangsw09@gmail.com>
2022-06-29 12:16:52 -0400

    Add clone method: clonal_trajectory and clone statistics

--- cospar/tool/_clone.py
+++ cospar/tool/_clone.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import scipy.sparse as ssp
 import scipy.stats as stats
+import seaborn as sns
 import statsmodels.sandbox.stats.multicomp
 from ete3 import Tree
 from matplotlib import pyplot as plt
@@ -313,3 +314,172 @@ def get_normalized_coarse_X_clone_v1(adata, selected_fates):
     df_X_cluster.index = new_fate_list
 
     return df_X_cluster
+
+
+def add_clone_id_for_each_cell(adata):
+    clone_id = list(np.zeros(adata.shape[0], dtype=str))
+    X_clone_old = adata.obsm["X_clone"].A
+    for j in range(adata.shape[0]):
+        clone_id[j] = ",".join(np.nonzero(X_clone_old[j, :])[0].astype(str))
+    adata.obs["clone_id"] = clone_id
+    logg.info("add information at obs['clone_id']")
+
+
+def remove_multiclone_cell(
+    adata, clone_threshold=2, keep_discarded_clones=True, update_X_clone=True
+):
+    """
+    cells with clones >= clone_threshold will be removed (set to 0) from the X_clone.
+    """
+    barcode_N_per_cell = adata.obsm["X_clone"].sum(1).A.flatten()
+    print("Multiclone cell fraction:", (barcode_N_per_cell > 1).mean())
+    print(
+        "Fraction of clones related to a multiclone cell:",
+        (adata.obsm["X_clone"][barcode_N_per_cell > 1].sum(0) > 0).mean(),
+    )
+    X_clone_new = adata.obsm["X_clone"].A
+    for x in np.nonzero(barcode_N_per_cell >= clone_threshold)[0]:
+        for j in range(X_clone_new.shape[1]):
+            X_clone_new[x, j] = 0
+
+    absent_clone_before = np.nonzero(adata.obsm["X_clone"].A.sum(0) == 0)[0]
+    lost_clones = np.nonzero(X_clone_new.sum(0) == 0)[0]
+    removed_clones = set(lost_clones).difference(absent_clone_before)
+    removed_clones = list(removed_clones)
+
+    if keep_discarded_clones:
+        logg.info("Add back dispeared clones due to cell removal")
+        X_clone_new[:, removed_clones] = adata.obsm["X_clone"].A[:, removed_clones]
+
+    adata.obsm["X_clone_old"] = adata.obsm["X_clone"]
+    if update_X_clone:
+        adata.obsm["X_clone"] = ssp.csr_matrix(X_clone_new)
+        logg.info("Updated X_clone")
+        logg.info(f"Check removed clones with adata.obsm['X_clone_old']")
+
+    barcode_N_per_cell_new = X_clone_new.sum(1)
+    df = pd.DataFrame(
+        {
+            "Clone_N_per_cell_old": barcode_N_per_cell,
+            "Clone_N_per_cell_new": barcode_N_per_cell_new,
+        }
+    ).melt()
+    fig, ax = plt.subplots()
+    ax = sns.histplot(data=df, x="value", hue="variable")
+    ax.set_xlabel("Clone number per cell")
+
+    return removed_clones
+
+
+def clone_statistics(adata):
+    """
+    Extract the number of clones and clonal cells for each time point
+    """
+
+    df = (
+        pd.DataFrame(adata.obsm["X_clone"].A)
+        .reset_index()
+        .rename(columns={"index": "cell_id"})
+        .assign(time_info=list(adata.obs["time_info"]))
+        .set_index(["cell_id", "time_info"])
+        .melt(ignore_index=False)
+        .reset_index()
+        .rename(columns={"variable": "clone_id", "value": "count"})
+        .query("count>0")
+    )
+
+    df = df.groupby("clone_id").agg(
+        cell_number=("cell_id", "count"),
+        time_points=("time_info", lambda x: ",".join(list(set(x)))),
+        time_point_N=("time_info", lambda x: len(set(x))),
+    )
+
+    df_clone = (
+        df.groupby("time_points")
+        .agg(clone_N=("time_points", "count"))
+        .assign(clone_fraction=lambda x: x["clone_N"] / x["clone_N"].sum())
+    )
+
+    df_cell = (
+        df.groupby("time_points")
+        .agg(cell_N=("cell_number", "sum"))
+        .assign(cell_fraction=lambda x: x["cell_N"] / x["cell_N"].sum())
+    )
+    print(df_clone)
+    print("-----------")
+    print(df_cell)
+    return df.reset_index()
+
+
+def computer_sister_cell_distance(
+    adata, selected_time=None, method="2D", key="X_emb", neighbor_number=10
+):
+    """
+    Parameters
+    ----------
+        selected_time:
+            selected time point
+        method:
+            {'2D','high'} using 2D embedding or high-dimension state measurement X_pca.
+        key:
+            If method='2D', decide which 2-D embedding to use. Could be 'X_umap', or 'X_emb' etc.
+        neighbor_number:
+            If method='high', decide the neighbor_number to construct the KNN graph.
+
+    Returns
+    -------
+        df_distance:
+            A dataframe for cell-cell distance
+    """
+
+    from scipy.spatial.distance import pdist, squareform
+
+    if selected_time is None:
+        adata_t1 = adata
+    else:
+        adata_t1 = adata[adata.obs["time_info"] == selected_time]
+
+    if method == "2D":
+        print("Use 2-dimension embedding")
+        X = adata_t1.obsm[key]
+        norm_distance = squareform(pdist(X))
+
+    elif method == "high":
+        print("Use high-dimension")
+        norm_distance = hf.compute_shortest_path_distance(
+            adata_t1, normalize=False, num_neighbors_target=neighbor_number
+        )
+    else:
+        raise ValueError("method should be among {'2D' or 'high'}")
+
+    t1_clone_size = adata_t1.obsm["X_clone"].A.sum(0)
+    selected_clone_idx = np.nonzero(t1_clone_size >= 2)[0]
+    distance_list = []
+    for x in selected_clone_idx:
+        ids_all = np.nonzero(adata_t1.obsm["X_clone"][:, x].A.flatten() > 0)[0]
+        distance_tmp = []
+        for i in range(len(ids_all)):
+            for j in range(i + 1, len(ids_all)):
+                dis = norm_distance[ids_all[i], ids_all[j]]
+                distance_tmp.append(dis)
+        distance_list.append(np.mean(distance_tmp))
+
+    df_distance = pd.DataFrame(
+        {
+            "clone_id": selected_clone_idx,
+            "clone_distance": distance_list,
+            "random_distance": np.random.choice(
+                norm_distance.flatten(), len(distance_list)
+            ),
+        }
+    )
+
+    fig, ax = plt.subplots()
+    ax = sns.histplot(df_distance["clone_distance"], bins=20)
+    ax.set_xlabel("Sister cell distance")
+    x = np.mean(norm_distance.flatten())
+    plt.plot([x, x], [0, 12], "-r")
+    # ax.text(x,10, 'mean random dist',fontsize=15,color='r')
+    below_random = np.mean(df_distance["clone_distance"] < x)
+    ax.set_title(f"t={selected_time}, below random: {below_random:.2f}")
+    return df_distance
